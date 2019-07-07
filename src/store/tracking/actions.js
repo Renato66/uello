84cea95bb7aabaa0d66dfc63165b88becdf3258c
Renato66 <revi66@hotmail.com>
2019-07-07 01:41:49 -0300

    clear up

--- src/store/tracking/actions.js
+++ src/store/tracking/actions.js
@@ -5,22 +5,18 @@ export default {
     const { data: result } = await axios.get('https://api.myjson.com/bins/zw5av')
     const trackIndex = payload | Math.floor(Math.random() * result.length)
     commit('SET_TRAKING_DETAILS', result[trackIndex])
-    if (getters.details.driver) {
+    if (getters.details.driver && getters.details.status !== 'Pedido coletado') {
       dispatch('estimateArrival')
     }
   },
   estimateArrival: async ({ commit, getters }, payload) => {
-    const origins = `${getters.details.address.geolocation.latitude},${getters.details.address.geolocation.longitude}`
-    const destinations = `${getters.details.driver.geolocation.latitude},${getters.details.driver.geolocation.longitude}`
-    // const { data: result } = await axios.get('https://api.myjson.com/bins/n2oq3')
-    const { data: result } = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
-      params: {
-        language: 'pt-BR',
-        origins,
-        destinations,
-        key: process.env.VUE_APP_GOOGLE_API_KEY
-      }
-    })
+    // const origins = `${getters.details.address.geolocation.latitude},${getters.details.address.geolocation.longitude}`
+    // const destinations = `${getters.details.driver.geolocation.latitude},${getters.details.driver.geolocation.longitude}`
+    // const { data: result } = await axios.post('some/backend/api' {
+    //   origins,
+    //   destinations
+    // })
+    const { data: result } = await axios.get('https://api.myjson.com/bins/n2oq3')
     commit('SET_TRAKING_ESTIMATE_ARRIVAL', result.rows[0].elements[0])
   }
 }
