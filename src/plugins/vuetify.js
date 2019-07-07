98caf26024c0ac7498f622db7f4030b070a4477d
Renato66 <revi66@hotmail.com>
2019-07-06 23:45:49 -0300

    primary color added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,9 +4,9 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
+    primary: '#e63b4f',
     secondary: '#424242',
-    accent: '#82B1FF',
+    accent: '#04baef',
     error: '#FF5252',
     info: '#2196F3',
     success: '#4CAF50',
